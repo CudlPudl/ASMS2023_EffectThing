31e4e7ca8184362d625443970eeebeca67613521
Jaakko <x@hax.ax>
2023-08-05 22:42:00 +0300

    scaled staff

--- Assets/Scripts/Creature/CreatureMovement.cs
+++ Assets/Scripts/Creature/CreatureMovement.cs
@@ -6,9 +6,10 @@ public class CreatureMovement : MonoBehaviour
 {
     [SerializeField] private Creature creature;
     [SerializeField] private GoalAction onForcebounds;
+    [SerializeField] private float speedMultiplier = 1.0f;
     public void Move(Vector3 amount)
     {
-        transform.position += amount;
+        transform.position += amount * speedMultiplier;
         if (transform.position.x > 39.5f || transform.position.x < -39.5f || transform.position.z > 39.5f || transform.position.z < -39.5f)
         {
             transform.position = new Vector3(Mathf.Clamp(transform.position.x, -39.5f, 39.5f), 0f, Mathf.Clamp(transform.position.z, -39.5f, 39.5f));
